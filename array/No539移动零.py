2039ef46c3ea082d00e6e4706fc8d5535ce2bdc6
rany1 <421313618@qq.com>
2017-09-12 18:30:09 +0800

    Create No539移动零.py

---
+++ array/No539移动零.py
@@ -0,0 +1,22 @@
+class Solution:
+    """
+    给一个数组 nums 写一个函数将 0 移动到数组的最后面，非零元素保持原数组的顺序
+    注意事项
+        1.必须在原数组上操作
+        2.最小化操作数
+    给出 nums = [0, 1, 0, 3, 12], 调用函数之后, nums = [1, 3, 12, 0, 0].
+    @param: nums: an integer array
+    @return:
+    """
+    def moveZeroes(self, nums):
+        # write your code here
+        pos=-1;
+        for index in range(0,len(nums)):
+            if nums[index]==0:
+                if pos==-1:
+                    pos=index;
+            else:
+                if pos>=0:
+                    nums[pos]=nums[index];
+                    nums[index]=0;
+                    pos=pos+1;
