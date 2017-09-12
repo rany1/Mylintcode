0f6727ab1f542fe780a52fcb57c6bd29a9594f8b
rany1 <421313618@qq.com>
2017-09-12 18:34:33 +0800

    Update No397最长上升连续子序列.py

--- array/No397最长上升连续子序列.py
+++ array/No397最长上升连续子序列.py
@@ -2,6 +2,7 @@
 
 class Solution:
     """
+    http://www.lintcode.com/zh-cn/problem/longest-increasing-continuous-subsequence/
     #给定一个整数数组（下标从 0 到 n-1， n 表示整个数组的规模），请找出该数组中的最长上升连续子序列。（最长上升连续子序列可以定义为从右到左或从左到右的序列。）
     给定 [5, 4, 2, 1, 3], 其最长上升连续子序列（LICS）为 [5, 4, 2, 1], 返回 4.
     给定 [5, 1, 2, 3, 4], 其最长上升连续子序列（LICS）为 [1, 2, 3, 4], 返回 4.
